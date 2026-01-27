7cb2d5cc0c6639d4607bfe8003c4bb2832ecac91
Josh Wagstaff <joshuawagstaff04@gmail.com>
2026-01-26 18:24:29 -0700

    W03 Individual Activity: Expand Inventory

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { loadHeaderFooter, getParam } from './utils.mjs';
+
+loadHeaderFooter();
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const listElement = document.querySelector('.product-list');
+
+const productList = new ProductList(category, dataSource, listElement);
+productList.init();
+
+document.querySelector('.products h2').textContent = `Top Products: ${category}`;
