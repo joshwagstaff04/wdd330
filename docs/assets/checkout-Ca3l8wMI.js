2d0a780ff9eecb4ce258eb3493bdb0447ea6bd0a
Josh Wagstaff <joshuawagstaff04@gmail.com>
2026-01-26 18:33:04 -0700

    Build for deployment

---
+++ docs/assets/checkout-Ca3l8wMI.js
@@ -0,0 +1,1 @@
+import{l as o}from"./utils-BtmlJ1Ha.js";o();
---
+++ docs/assets/productListing-BfRSetnK.js
@@ -0,0 +1,11 @@
+import{r as c,l as i,a as o}from"./utils-BtmlJ1Ha.js";import{P as n}from"./ProductData-8k7b23-H.js";function d(t){return`<li class="product-card">
+    <a href="../product_pages/?product=${t.Id}">
+      <img
+        src="${t.Images.PrimaryMedium}"
+        alt="${t.Name}"
+      />
+      <h3 class="card__brand">${t.Brand.Name}</h3>
+      <h2 class="card__name">${t.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${t.FinalPrice}</p>
+    </a>
+  </li>`}class l{constructor(a,r,s){this.category=a,this.dataSource=r,this.listElement=s}async init(){const a=await this.dataSource.getData(this.category);this.renderList(a)}renderList(a){c(d,this.listElement,a)}}i();const e=o("category"),m=new n,u=document.querySelector(".product-list"),h=new l(e,m,u);h.init();document.querySelector(".products h2").textContent=`Top Products: ${e}`;
